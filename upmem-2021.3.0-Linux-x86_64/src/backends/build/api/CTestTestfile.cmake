6bba78df7b47e2d1dfb15eab9cdd6b3a0d574e96
Yun-Ze Li <p76091292@gs.ncku.edu.tw>
2023-07-18 07:53:11 +0000

    remove CMake directory from the repo
    Signed-off-by: Yun-Ze Li <p76091292@gs.ncku.edu.tw>

--- upmem-2021.3.0-Linux-x86_64/src/backends/build/api/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/membo/upmem-lib-membo/upmem-2021.3.0-Linux-x86_64/src/backends/api
-# Build directory: /home/membo/upmem-lib-membo/upmem-2021.3.0-Linux-x86_64/src/backends/build/api
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
