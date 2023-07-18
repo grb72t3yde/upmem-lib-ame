7fd5748998f86f848886195c118d25ddd8779ae8
Yun-Ze Li <p76091292@gs.ncku.edu.tw>
2023-07-18 07:46:32 +0000

    change to root directory of the repo to upmem-lib-membo
    Signed-off-by: Yun-Ze Li <p76091292@gs.ncku.edu.tw>

---
+++ upmem-2021.3.0-Linux-x86_64/src/backends/build/ufi/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/membo/upmem-lib-membo/upmem-2021.3.0-Linux-x86_64/src/backends/ufi
+# Build directory: /home/membo/upmem-lib-membo/upmem-2021.3.0-Linux-x86_64/src/backends/build/ufi
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
